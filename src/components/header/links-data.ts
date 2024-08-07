bdd13ad3bdcd0da4ad2077ca5bec452285c1ad6d
StevenMolina22 <stevenmolina2205@gmail.com>
2024-08-07 20:13:01 -0300

    Organizes home 2 into sections

---
+++ src/components/header/links-data.ts
@@ -0,0 +1,179 @@
+export const homeDropdownLinks = [
+  {
+    label: "Home 1",
+    href: "index.html",
+  },
+  {
+    label: "Home 1 RTL",
+    href: "index-rtl.html",
+  },
+  {
+    label: "Home 2",
+    href: "home-2.html",
+  },
+  {
+    label: "Home 3",
+    href: "home-3.html",
+  },
+  {
+    label: "Home 4",
+    href: "home-4.html",
+  },
+  {
+    label: "Home 5",
+    href: "home-5.html",
+  },
+  {
+    label: "Home 6",
+    href: "home-6.html",
+  },
+  {
+    label: "Crypto Consultant",
+    href: "home-7.html",
+  },
+  {
+    label: "NFT Game",
+    href: "home-8.html",
+  },
+  {
+    label: "DAO Platform",
+    href: "home-9.html",
+  },
+  {
+    label: "Crypto App",
+    href: "home-10.html",
+  },
+  {
+    label: "Crypto Trading",
+    href: "home-11.html",
+  },
+  {
+    label: "ICO Landing",
+    href: "home-12.html",
+  },
+  {
+    label: "NFT Aggregator",
+    href: "home-13.html",
+  },
+];
+
+export const pagesDropdownLinks = [
+  {
+    label: "Maintenance",
+    href: "maintenance.html",
+  },
+  {
+    label: "Case Studies",
+    href: "case-studies.html",
+  },
+  {
+    label: "Single Case Study",
+    href: "single-case-study.html",
+  },
+  {
+    label: "Careers",
+    href: "careers.html",
+  },
+  {
+    label: "Item Details",
+    href: "item.html",
+  },
+  {
+    label: "Collections Wide",
+    href: "collections-wide-sidebar.html",
+  },
+  {
+    label: "Collections",
+    href: "collections.html",
+  },
+  {
+    label: "Collection",
+    href: "collection.html",
+  },
+  {
+    label: "Activity",
+    href: "activity.html",
+  },
+  {
+    label: "Rankings",
+    href: "rankings.html",
+  },
+  {
+    label: "User",
+    href: "user.html",
+  },
+  {
+    label: "Edit Profile",
+    href: "edit-profile.html",
+  },
+  {
+    label: "About",
+    href: "about.html",
+  },
+  {
+    label: "Contact",
+    href: "contact.html",
+  },
+  {
+    label: "Wallet",
+    href: "wallet.html",
+  },
+  {
+    label: "Login",
+    href: "login.html",
+  },
+  {
+    label: "Page 404",
+    href: "404.html",
+  },
+  {
+    label: "Terms of Service",
+    href: "tos.html",
+  },
+];
+
+export const exploreLinks = [
+  {
+    href: "collections.html",
+    label: "All NFTs",
+  },
+  {
+    href: "collections.html",
+    label: "Art",
+  },
+  {
+    href: "collections.html",
+    label: "Collectibles",
+  },
+  {
+    href: "collections.html",
+    label: "Music",
+  },
+];
+
+export const resourcesLinks = [
+  {
+    href: "help-center.html",
+    label: "Help Center",
+  },
+  {
+    href: "platform-status.html",
+    label: "Platform Status",
+  },
+  {
+    href: "partners.html",
+    label: "Partners",
+  },
+  {
+    href: "blog.html",
+    label: "Blog",
+  },
+  {
+    href: "single-post.html",
+    label: "Single Post",
+  },
+  {
+    href: "newsletter.html",
+    label: "Newsletter",
+  },
+];
